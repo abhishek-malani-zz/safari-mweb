646a69e15ebb1c7c7ee02b35e64880af52993c8c
abhishek.malani <abhishekmalani3@gmail.com>
2019-11-07 18:11:50 +0530

    segregated code to different files

--- Utilities/DriverManager.py
+++ Utilities/DriverManager.py
@@ -5,10 +5,13 @@ class AppTestAppium:
     @staticmethod
     def instantiate_driver():
         desired_caps = {'platformName': 'iOS',
-                        'platformVersion': '12.4',
-                        'deviceName': 'iPhone XR',
+                        'platformVersion': '13.2',
+                        'deviceName': 'iPhone 11',
                         'automationName': 'xcuitest',
                         'bundleId': 'com.apple.mobilesafari',
                         }
         driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         return driver
+
+    def tearDown(self):
+        self.driver.quit()
